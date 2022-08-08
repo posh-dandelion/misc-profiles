5ec04c80854772017e3fc96eccbe5acf05791d78
posh-dandelion <dandelion@katzen.haus>
2022-08-07 18:13:34 -0600

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,22 @@
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme paradox
+
+Set-Item -Path function:\kali -Value {
+    if ((Get-Service vmms).Status -ne "Running"){Start-Service vmms}
+    if ((Get-VM kali).State -ne "Running"){Start-VM kali}
+    Remove-Variable kali -ea 0 -Scope Global
+    while ($kali -notmatch "^1((7|9)2|0)"){
+        Write-Host "`r$kali" -NoNewline
+        Set-Variable kali -Scope Global -Value $(Get-VM kali|Get-VMNetworkAdapter).IPAddresses[0]
+    }
+    Write-Host $kali -NoNewline
+    Start-Process PowerShell -ArgumentList "-WindowStyle Minimized -Command ssh dandelion@$kali `"tar czvf /tmp/dandelion.tgz /home/dandelion/`";scp dandelion@$kali`:/tmp/dandelion.tgz `"D:\Backups\$(get-date -Format 'yyyyMMddHHmmss')_dandelion_kali.tgz`";ssh dandelion@$kali `"rm /tmp/dandelion.tgz`""
+    ssh dandelion@$kali
+}
+
+function ilak {
+    if ((Get-VM kali).State -eq "Running"){Stop-VM kali}
+    if ((Get-Service vmms).Status -eq "Running"){Stop-Service vmms}
+}
+
+Invoke-RestMethod -Uri https://wttr.in/Rapid_City
